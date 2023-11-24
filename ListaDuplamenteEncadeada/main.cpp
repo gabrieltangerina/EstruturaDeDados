8c9325202995b7707b5af2aa7f38e5f8ab5358cf
Gabriel Tangerina <110418376+gabrieltangerina@users.noreply.github.com>
2023-11-24 10:17:45 -0300

    Lista Duplamente Encadeada

---
+++ ListaDuplamenteEncadeada/main.cpp
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/cppFiles/main.cc to edit this template
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: aluno
+ *
+ * Created on 24 de novembro de 2023, 09:07
+ */
+
+#include <cstdlib>
+
+#include "cListaDuplamente.h"
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    cListaDuplamente obj;
+    obj.menu();
+    
+    return 0;
+}
+
