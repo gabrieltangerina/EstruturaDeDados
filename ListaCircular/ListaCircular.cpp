8303837e8b91d9fe541907256c936abb7a8b41d5
Gabriel Tangerina <110418376+gabrieltangerina@users.noreply.github.com>
2023-11-30 22:32:59 -0300

    Atualizando código

--- ListaCircular/ListaCircular.cpp
+++ ListaCircular/ListaCircular.cpp
@@ -1,45 +1,21 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/cppFiles/class.cc to edit this template
- */
-
-/* 
- * File:   ListaDuplamente.cpp
- * Author: aluno
- * 
- * Created on 24 de novembro de 2023, 08:16
- */
-
-#include "ListaCircular.h"
-#include <iostream>
-
-using namespace std;
-
 ListaCircular::ListaCircular() {
-    
     this->aux = NULL;
     this->inicio = NULL;
     this->fim = NULL;
     this->tamanhoLista = 0;
-    
 }
 
-ListaCircular::ListaCircular(const ListaCircular& orig) {
-}
+void ListaCircular::menu() {
 
-ListaCircular::~ListaCircular() {
-}
-
-void ListaCircular::menu(){
-    
     int opc;
     cout << endl << "1 - Adicionar" << endl;
     cout << "2 - Listar" << endl;
+    cout << "3 - Remover" << endl;
     cout << "0 - Sair" << endl;
     cout << "Opcao: ";
     cin >> opc;
-    
-    switch(opc){
+
+    switch (opc) {
         case 1:
             this->inserir();
             this->menu();
@@ -48,40 +24,79 @@ void ListaCircular::menu(){
             this->listar();
             this->menu();
             break;
-        case 0:
+        case 3:
+            this->remover();
+            this->menu();
+            break;
+        case 0:;
             cout << "Saindo" << endl;
             break;
     }
 }
 
-void ListaCircular::inserir(){
+void ListaCircular::inserir() {
     int num;
-    cout << "Informe o número que será inserido na lista: ";
+    cout << "Informe o número que deseja inserir: ";
     cin >> num;
-    
-    this->aux = (struct no*)malloc(sizeof(aux));
-    
-    this->aux->prox = NULL;
+
+    this->aux = (struct no*) malloc(sizeof (aux));
+
     this->aux->num = num;
-    
-    if(this->inicio == NULL){
+    this->aux->ant = NULL;
+    this->aux->prox = NULL;
+
+    if (this->inicio == NULL) {
         this->inicio = this->aux;
-        this->inicio->prox = NULL;
-    }else{
+    } else {
         this->fim->prox = this->aux;
-        this->aux->ant = this->fim;
     }
-    
+
+    this->aux->ant = this->fim;
     this->fim = this->aux;
-    this->inicio->ant = this->fim;
     this->fim->prox = this->inicio;
     this->tamanhoLista++;
 }
 
-void ListaCircular::listar(){
+void ListaCircular::listar() {
     int contador = 0;
-    for(this->aux = this->inicio; this->tamanhoLista > contador; this->aux = this->aux->prox){
-        cout << this->aux->num << "   ";
+    cout << "Elementos: ";
+    for (this->aux = this->inicio; this->tamanhoLista > contador; this->aux = this->aux->prox) {
         contador++;
+        cout << this->aux->num << "   ";
+    }
+    cout << endl;
+}
+
+void ListaCircular::remover() {
+    int chave, contador = 0;
+    cout << "Informe o elemento que deseja remover: ";
+    cin >> chave;
+
+    bool encontrado = false;
+
+    for (this->aux = this->inicio; this->tamanhoLista > contador ; this->aux = this->aux->prox) {
+        contador++;
+        if (this->aux->num == chave) {
+            if (this->aux == this->fim) {
+                this->aux->ant->prox = this->inicio;
+                this->aux->prox->ant = this->aux->ant;
+                this->fim = this->fim->ant;
+            } else if (this->aux == this->inicio) {
+                this->aux->prox->ant = this->aux->ant;
+                this->aux->ant->prox = this->aux->prox;
+                this->inicio = this->inicio->prox;
+            } else {
+                this->aux->ant->prox = this->aux->prox;
+                this->aux->prox->ant = this->aux->ant;
+            }
+
+            free(this->aux);
+            cout << "Elemento removido com sucesso!" << endl;
+            encontrado = true;
+        }
+    }
+
+    if (!encontrado) {
+        cout << "Elemento não encontrado" << endl;
     }
 }
